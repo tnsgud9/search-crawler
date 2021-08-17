91d47ba61ce33dd9c6578beef491bc5230894363
tnsgud9 <tnsgud9@naver.com>
2021-08-17 20:07:12 +0900

    update crawler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
-import { Crawler } from "./lib/crawler";
+import { CrawlerCoordinator } from "./libs/crawlerCoordinator";
+const text = `<body>
+<a href="https://naver.com">hello</a>
+<div>sdfaasdf</div>
+<a href="https://kakao.com"> hello world</a>
+</body>
+`;
+const matched = text.match(/<a\s+(?:[^>]*?\s+)?href=(["'])(.*?)\1>/i);
+console.log(matched);
+const multipleMatched = text.match(/<a\s+(?:[^>]*?\s+)?href=(["'])(.*?)\1>/g);
+console.log(multipleMatched);
 
 (async () => {
-  const crawler = new Crawler("https://www.naver.com");
-  await crawler.trip();
+  const coodinator = new CrawlerCoordinator();
+  coodinator.reportUrl("https://naver.com");
+  await coodinator.start();
 })();
